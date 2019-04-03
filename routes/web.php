0b31a0d7617b6ce2fc89fda38bfc097c42c2fb87
SE.Selfish <s1B101402816@gmail.com>
2019-04-03 01:49:41 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 
 Route::get('/', function () {
     return view('welcome');
